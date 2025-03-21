c8d0979e8a3c7d8c8835de34359d955cde9b0b30
omid <omid.firoozi2000@gmail.com>
2025-03-21 13:48:34 +0430

    Finalized the project

--- youtube_video_downloader/settings.py
+++ youtube_video_downloader/settings.py
@@ -76,10 +76,7 @@ WSGI_APPLICATION = 'youtube_video_downloader.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': {}
 }
 
 
